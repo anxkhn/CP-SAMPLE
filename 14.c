bd7039eaaa7f65706ca868b49c0e69e63bd44839
Anas Khan <83116240+anxkhn@users.noreply.github.com>
2022-06-17 20:49:35 +0530

    Add files via upload

---
+++ 14.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+void main()
+{
+	int n, i, a[100], even=0, odd=0;
+	printf("Enter total number of integers: ");
+	scanf("%d", &n);
+	for (i = 0; i <= n - 1; i++)
+	{
+		printf("Enter a value: ");
+		scanf("%d", &a[i]);
+		if (a[i]%2==0)
+            even+=1;
+        else
+            odd+=1;
+	}
+	printf("The number of even integers entered are ", even);
+	printf("\nThe number of odd integers entered are ", odd);
+}
