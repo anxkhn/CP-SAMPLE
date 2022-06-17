6824f4f0b47847518f8c9e40fa3c3757ee982efd
Anas Khan <83116240+anxkhn@users.noreply.github.com>
2022-06-17 20:41:52 +0530

    Add files via upload

---
+++ 13.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+void main()
+{
+	int n, i, a[100], sum = 0;
+	float avg;
+	printf("Enter total number of integers ");
+	scanf("%d", &n);
+	for (i = 0; i <= n - 1; i++)
+	{
+		printf("Enter a value: ");
+		scanf("%d", &a[i]);
+	}
+	for (i = 0; i <= n - 1; i++)
+	{
+		sum = sum + a[i];
+	}
+	avg = sum;
+	avg = avg / n;
+	printf("The average of the numbers entered is %.4f", avg);
+}
