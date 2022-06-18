96aed64a3defecd7a92157b060e3b9d44f6ab213
Anas Khan <83116240+anxkhn@users.noreply.github.com>
2022-06-18 17:06:15 +0530

    Add files via upload

---
+++ 23.c
@@ -0,0 +1,21 @@
+#include <conio.h>
+#include <stdio.h>
+
+void swap(int, int);
+void main()
+{
+	int x, y;
+	printf("Enter x and y ");
+	scanf("%d%d", &x, &y);
+	printf("\nNumbers before swapping x = %d and y = %d.", x, y);
+	swap(x, y);
+}
+
+void swap(int x, int y)
+{
+	int temp;
+	temp = x;
+	x = y;
+	y = temp;
+	printf("\nNumber after swapping x = %d and y = %d.", x, y);
+}
