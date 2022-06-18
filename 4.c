be6d4687a0480f9c905319dd31231f60016ef516
Sumil Suthar <102129056+SumilSuthar197@users.noreply.github.com>
2022-06-18 20:23:37 +0530

    Update 4.c

--- 4.c
+++ 4.c
@@ -19,3 +19,29 @@ void main()
 		}
 	}
 }
+
+/* ALTERNATIVE approach
+
+#include <stdio.h>
+#include <conio.h>
+
+int main()
+{
+	int n,i,count;
+	for (n = 75; n <= 150; n++)
+	{
+        count=0;
+	for(i=2;i<=n;i++)
+        { 
+            if(n%i==0)
+            count++;
+        }
+        if(count==1)
+        {
+            printf("%d ",n);
+        }
+	}
+
+} 
+
+*/
