171fdc76643ac8999fdc0d7410c893c8927fcae1
Anas Khan <83116240+anxkhn@users.noreply.github.com>
2022-06-18 16:58:20 +0530

    Add files via upload

--- 22.c
+++ 22.c
@@ -1,24 +1,43 @@
-#include<stdio.h>
-int summation(int x[],int y[],int z[],int n)
-{
-    int i,sum=0;
-    for(i=0;i<n;i++)
-    {
-        sum=sum+x[i]*y[i]*z[i];
-    }
-    return sum;
-}
+#include <conio.h>
+#include <stdio.h>
+
+void sum(int x[], int y[], int z[], int n);
 void main()
 {
-    int i,n,x[100],y[100],z[100],result;
-    printf("enter the value of n: ");
-    scanf("%d",&n);
-    for(i=0;i<n;i++)
-    {
-        printf("Enter x%d,y%d and z%d: ",i+1,i+1,i+1);
-        scanf("%d%d%d",&x[i],&y[i],&z[i]);
-    }
-    result=summation(x,x,y,n)+summation(x,y,y,n)+summation(x,y,z,n);
-    printf("the solution of equation is %d",result);
-    
+	int x[100], y[100], z[100], n, i;
+	printf("Enter n \n");
+	scanf("%d", &n);
+	printf("\nEnter the values of x \n");
+	for (i = 1; i <= n; i++)
+	{
+		scanf("%d", &x[i]);
+	}
+
+	printf("Enter the values of y \n");
+	for (i = 1; i <= n; i++)
+	{
+		scanf("%d", &y[i]);
+	}
+
+	printf("Enter the values of z \n");
+	for (i = 1; i <= n; i++)
+	{
+		scanf("%d", &z[i]);
+	}
+
+	sum(x, y, z, n);
+}
+
+void sum(int x[], int y[], int z[], int n)
+{
+	int s1, s2, s3, sum = 0, i;
+	for (i = 1; i <= n; i++)
+	{
+		s1 = (x[i] *x[i] *y[i]);
+		s2 = (y[i] *y[i] *x[i]);
+		s3 = (x[i] *y[i] *z[i]);
+		sum = sum + s1 + s2 + s3;
+	}
+
+	printf("T= %d", sum);
 }
