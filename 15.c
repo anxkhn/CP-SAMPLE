0956478b2438bc3ad4fdea8c70a664f8c2f04fc0
Anas Khan <83116240+anxkhn@users.noreply.github.com>
2022-06-18 22:19:46 +0530

    Update 15.c

--- 15.c
+++ 15.c
@@ -2,22 +2,23 @@
 
 void main()
 {
-	int i, j, n;
-	printf("Enter n = ");
+	int n, i, a[100];
+	printf("Enter a number :\n");
 	scanf("%d", &n);
-	int a[n];
 	for (i = 0; i < n; i++)
 	{
-		printf("\n Enter the elements: ");
+		printf("Enter the value \n");
 		scanf("%d", &a[i]);
 	}
 	int max = a[0];
-	for (i = 0; i < n; i++)
 	{
-		if (a[i] > max)
+		for (i = 0; i < n; i++)
 		{
-			max = a[i];
+			if (a[i] > max)
+			{
+				max = a[i];
+			}
 		}
+		printf("the largest value is %d", max);
 	}
-	printf("\n the largest element in array is %d",max);
 }
