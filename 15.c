1c6f6496d42b86b670e1eecca76c45dd821be0c8
Anas Khan <83116240+anxkhn@users.noreply.github.com>
2022-06-18 16:07:35 +0530

    Update 15.c

--- 15.c
+++ 15.c
@@ -6,7 +6,6 @@ void main()
 	printf("Enter n = ");
 	scanf("%d", &n);
 	int a[n];
-	printf("Enter elements  \n");
 	for (i = 0; i < n; i++)
 	{
 		scanf("%d", &a[i]);
@@ -15,7 +14,8 @@ void main()
 	for (i = 0; i < n; i++)
 	{
 		if (a[i] > max)
-		{
+		{	
+			printf("Enter elements  \n");
 			max = a[i];
 		}
 	}
