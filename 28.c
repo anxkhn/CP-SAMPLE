ef3f0fe228ecfc148fa0d6464c10e5afb704e4ff
Anas Khan <83116240+anxkhn@users.noreply.github.com>
2022-06-18 22:36:50 +0530

    Update 28.c

--- 28.c
+++ 28.c
@@ -1,35 +1,24 @@
-// Write a program that checks whether the entered string is a palindrome or not, without using string header file.
-
 #include <stdio.h>
-#include <string.h>
 
 void main()
 {
-    char string[25], reverse_string[25] = {'\0'};
-    int i, length = 0, flag = 0;
-
-    printf("Enter a string \n");
-    gets(string);
-    /*  keep going through each character of the string till its end */
-    for (i = 0; string[i] != '\0'; i++)
+    int i;
+    char a[100];
+    printf("Enter the string: ");
+    gets(a);
+    int l=0;
+    while (a[l]!='\0')
     {
-        length++;
+        l++;
     }
-    printf("The length of the string '%s' = %d\n", string, length);
-    for (i = length - 1; i >= 0 ; i--)
+    printf("The length of string is %d",l);
+    for(i=0; i<=(l-1)/2;i++)
     {
-        reverse_string[length - i - 1] = string[i];
+        if (a[l-i-1]!=a[i])
+            break;
     }
-   /*  Check if the string is a Palindrome */
-
-    for (flag = 1, i = 0; i < length ; i++)
-    {
-        if (reverse_string[i] != string[i])
-            flag = 0;
-    }
-    if (flag == 1)
-       printf ("%s is a palindrome \n", string);
+    if(i==((l-1)/2)+1)
+        printf("\nThe string is plaindrome.");
     else
-       printf("%s is not a palindrome \n", string);
+        printf("\nThe string is not palindrome.");
 }
-
