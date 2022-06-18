f20a7143b4ee7f6b1c6176ed3d7e672f00a94c7a
Anas Khan <83116240+anxkhn@users.noreply.github.com>
2022-06-18 16:09:27 +0530

    Update 16.c

--- 16.c
+++ 16.c
@@ -1,20 +1,32 @@
-#include <conio.h>
 #include <stdio.h>
 
 void main()
 {
-	int number = 100, digit1, digit2, digit3, t;
-	while (number <= 999)
+	int i, j, n, temp;
+	printf("Enter n = ");
+	scanf("%d", &n);
+	int a[n];
+	printf("Enter elements \n");
+	for (i = 0; i < n; i++)
 	{
-		digit1 = number - ((number / 10) *10);
-		digit2 = (number / 10) - ((number / 100) *10);
-		digit3 = (number / 100) - ((number / 1000) *100);
-		t = (digit1 *digit1 *digit1) + (digit2 *digit2 *digit2) + (digit3 *digit3 *digit3);
-		if (t == number)
-		{
-			printf("%d \n", t);
-		}
+		scanf("%d", &a[i]);
+	}
 
-		number++;
+	for (i = 0; i < n; i++)
+	{
+		for (j = 0; j < n - 1; j++)
+		{
+			if (a[i] < a[j])
+			{
+				temp = a[i];
+				a[i] = a[j];
+				a[j] = temp;
+			}
+		}
+	}
+	printf("Ascending order is ");
+	for (i = 0; i < n; i++)
+	{
+		printf(" %d ", a[i]);
 	}
 }
